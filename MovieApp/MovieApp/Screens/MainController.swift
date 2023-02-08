f606678c4fee806f7d4a6b1fd068fe999e41b187
Almaz Sailau <almaz.saylau@gmail.com>
2023-02-08 22:11:15 +0600

    Project was created!

---
+++ MovieApp/MovieApp/Screens/MainController.swift
@@ -0,0 +1,18 @@
+//
+//  MainController.swift
+//  MovieApp
+//
+//  Created by Sailau Almaz Maratuly on 08.02.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+    }
+
+
+}
